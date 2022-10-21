9a23afd1bbc797f8d3289d023a37bc13a6da7a96
John Caruthers <john.d.caruthers@gmail.com>
2022-10-21 07:55:47 -0400

    Update genetic_algo.py

--- genetic_algo.py
+++ genetic_algo.py
@@ -3,14 +3,21 @@ import gen_chrom as gc
 
 distance_array = np.array([[0,25.1,21.9,31.7,45.4],[25.1,0,44.9,18,68.6],[21.9,44.9,0,50.7,27.7],[31.7,18,50.7,0,74.4],[45.4,68.6,27.7,74.4,0]])
 def gen_pop(num):
+    '''
+    This function generates a structured array of size "num", that contains a chromosome and fitness score initialized to 0.0.  Calls upon gen_chrom (gc)
+    to generate the permutation matrices (chromosome).  Returns the completed sub population containing "num" number of chromosomes. 
+    '''
     # build structured array (chromosome, fitness)
     dtype=[('chrom', np.ndarray),('fit', np.float32)]
+    
     # initialize first chromosome, assign a fitness of 0.0
     sub_pop = np.array([(gc.gen_permtx(5), 0.0)], dtype=dtype)
+    
     # append new (chromosome, fitness) to the sub_pop structured array, will total to "num" number of chromosome-fitness pairs
     # does not check for identical chromosomes, not too concerned with "twins"
     for i in range(num-1):
         sub_pop = np.append(sub_pop, np.array([(gc.gen_permtx(5), 0.0)],dtype=dtype))
+        
     return sub_pop
 
 
